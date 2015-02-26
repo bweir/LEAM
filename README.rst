da3359b1113de56bd8be120c3b15a875ae682edc
Brett Weir <brett@lamestation.com>
2015-02-25 21:17:46 -0800

    Added setup.py and reST README

---
+++ README.rst
@@ -0,0 +1,22 @@
+LEAM
+====
+
+LEAM can take LameStation games written with the LameStation SDK and
+turn them into games that can be run on your desktop.
+
+Want to share your awesome game with your friends but they don't have a
+LameStation? No problem! LEAM it up!
+
+.. figure:: screenshots/SpriteDemo.spin.py_015.png
+   :alt: gratuitous screenshot
+
+   gratuitous screenshot
+*SpriteDemo.spin running on Ubuntu.*
+
+The name
+~~~~~~~~
+
+It's pronounced LEEEEEEM! It's like Steam for LameStation. I think.
+
+LEAM isn't exactly a recursive acronym that standards for "LEAM isn't
+Exactly An eMulator."
---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+        long_description = f.read()
+
+setup(
+        name = "LEAM",
+        version = '0.1.0',
+        author = "LameStation",
+        author_email = "contact@lamestation.com",
+        description = "Play LameStation games on your desktop!",
+        long_description = long_description,
+        license = "GPLv3",
+        url = "https://github.com/lamestation/LEAM",
+        keywords = "emulator game converter lamestation desktop python spin",
+        packages=find_packages(),
+        include_package_data=True,
+        scripts=['bin/leam'],
+        classifiers=[
+            "Environment :: Console",
+            "Development Status :: 1 - Planning",
+            "Topic :: Games/Entertainment",
+            "Topic :: System :: Emulators",
+            "Topic :: Software Development :: Pre-processors",
+            "Topic :: Multimedia :: Graphics :: Viewers",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Programming Language :: Python :: 2 :: Only",
+            "Programming Language :: Python :: 2.7",
+            ]
+        )
